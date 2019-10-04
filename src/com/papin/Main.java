76673f8443a6807a605b1264c452da7519c4fcf1
Papin Faizal <papin97@gmail.com>
2019-10-04 09:47:51 +0700

    Update Main.java
    Testing processor detail

--- src/com/papin/Main.java
+++ src/com/papin/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        
+        SystemInfo si = new SystemInfo();
+        CentralProcessor cp = si.getHardware().getProcessor();
+        System.out.println("Processor codename: "+cp.getIdentifier());
     }
 }
